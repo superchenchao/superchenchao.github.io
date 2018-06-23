420e77c0503f47fe856ecc2f7ba7a30e241a71c3
chenchao <1129590014@qq.com>
2018-06-23 09:31:38 +0800

    0623

--- src/project.dev.886dc.js
+++ src/project.dev.886dc.js
@@ -328,8 +328,7 @@ require = function() {
         this.node.parent.zIndex = 2;
       },
       onCollisionStay: function onCollisionStay(other) {
-        cc.log("this.down_lock,,,,,", this.down_lock);
-        1 == other.tag || (2 == other.tag && this.down_lock ? cc.log("2222222222", other) : 3 == other.tag);
+        1 == other.tag || 2 == other.tag && this.down_lock || 3 == other.tag;
       },
       onTouchStart: function onTouchStart(event) {
         this.lock = true;
